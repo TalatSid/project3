b11ea1cf3f1a2b32cc2abcad8d167e5df7981365
Talat Fatima Siddiqui <talat.khasim@gmail.com>
2018-08-02 22:45:15 +0000

    index render - Django

--- orders/models.py
+++ orders/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     item = models.CharField(max_length = 64)
-    cost = models.DecimalField(decimal_places=2, max_digits=2)
+    cost = models.DecimalField(decimal_places=2, max_digits=5)
 
     def __str__(self):
         return f"{self.item} ({self.cost})"
